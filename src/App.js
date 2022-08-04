ee68c541277f2c92a767544d2e6f32373b94ab6c
junaidtkhan <89198741+junaidtkhan@users.noreply.github.com>
2022-08-04 12:04:26 +0500

    1204

--- src/App.js
+++ src/App.js
@@ -4,15 +4,41 @@ import { MovieComponent } from './MovieStore/Moviecomponents';
 import { Colorizer } from './colorize/colorize';
 import { Incrementer } from './Incrementer/incrementer';
 import ExpenseItem from './ExpensTracker/ExpenseItem';
+import { Expense } from './ExpensTracker/Expense';
 function App() {
 
-
-
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    {
+      id: 'e2',
+      title: 'New TV',
+      amount: 799.49,
+      date: new Date(2021, 2, 12)
+    },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
   return (
 
+
     <div className='App'>
-      <h2>Hello</h2>
-<ExpenseItem/>
+      <h2>React APP</h2>
+      <Expense items={expenses} />
     </div>
   );
 }
--- src/ExpensTracker/ExpenseItem.js
+++ src/ExpensTracker/ExpenseItem.js
@@ -1,6 +1,20 @@
-function ExpenseItem(){
-    return(
-<h2>Expense Item!</h2>
+import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate'
+function ExpenseItem(props) {
+
+    // const expenseDate=new Date(2012,2,28);
+    // const expenseTitle='Car Insurance'
+    // const expenseAmount=294.67
+    
+    return (
+        <div className="expense-item">
+            <ExpenseDate date={props.date}/>
+            <div className="expense-item__description">
+                <h2>{props.title}</h2>
+            </div>
+            <div className="expense-item__price">${props.amount}</div>
+        </div>
+
     )
 }
 export default ExpenseItem
